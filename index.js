dd3fae4e9450a500986db684d2374417001d6c38
Christopher Henderson <chenderson5280@gmail.com>
2020-08-20 00:39:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+   var  callOut = ""
+    for (let i = 0; i < dwarves.length; i++){
+       callOut += `${i+1}. ${dwarves[i]} `
+    }
+  return callOut
 }
-
 function summonCaptainPlanet(planeteerCalls){
+  var summonCall =[]
+  for (let i=0;i < planeteerCalls.length ; i++){
+       summonCall.push(`${planeteerCalls[i].toUpperCase()}!`)
+  }
+  return summonCall
 }
-
 function longPlaneteerCalls(words) {
+  if (words.length > 4){
+    return true
+  }
+  else {
+    return false
+  }
 }
-
 function findTheCheese (foods) {
+   var cheese = ['cheddar', 'gouda', 'camembert']
+   for (let i=0;i<foods.length ; i++){
+     for (let x=0;x < cheese.length; x++){
+    if (foods[i] === cheese [x]){
+      return cheese[x]
+    }
+    }
+
+  }
+  return  "no cheese!"
 }
